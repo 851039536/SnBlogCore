6eb971baf15220eb282f1974a372bf65e4cd1417
m-Kai.Ouyang <843766158@qq.com>
2024-02-27 10:56:22 +0800

    add

--- SnBlogCore/obj/Debug/net8.0/SnBlogCore.AssemblyInfo.cs
+++ SnBlogCore/obj/Debug/net8.0/SnBlogCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnBlogCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed05c751486ba7c70281baba79f453a08fff967e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnBlogCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnBlogCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
